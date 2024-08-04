cd5806264fdb95279c5fb30a8dd590d7e88d1379
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-04 10:56:46 +0100

    feat(eslint, tsconfig): Include tests in linting and tsconfig
    The changes in this commit include the 'test' directory in the eslint
    config and tsconfig.json. Previously, the 'test' directory was ignored
    by eslint, but now it will be linted. Additionally, the 'test' directory
    is now included in the tsconfig.json file, allowing TypeScript to
    transpile the test files.

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import { ryoppippi } from '@ryoppippi/eslint-config';
 export default ryoppippi({
 	tailwind: false,
 	svelte: true,
-	ignores: ['test/**', 'example/**'],
+	ignores: ['example/**'],
 	typescript: {
 		tsconfigPath: './tsconfig.json',
 	},
