e4c607bad6f915ee09a90f885c24c1b2a043857e
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-04 13:00:48 +0100

    refactor(test): update compiler parameters and add filename
    The compiler function in test/compiler.ts has been updated. The
    'cssOptions' parameter has been renamed to 'cssPreprocessorOptions' for
    better clarity. Additionally, a 'filename' option has been added to the
    preprocess function call, resolving to the path of 'App.svelte'.

--- test/compiler.ts
+++ test/compiler.ts
@@ -1,21 +1,25 @@
+import path from 'node:path';
 import { preprocess } from 'svelte/compiler';
 import { cssModules } from '../src';
 
 type Params = {
 	source: string;
-	cssOptions: Parameters<typeof cssModules>[0];
+	cssPreprocessorOptions: Parameters<typeof cssModules>[0];
 	preprocessOptions: Parameters<typeof preprocess>[2];
 };
 
 export async function compiler({
 	source,
-	cssOptions,
+	cssPreprocessorOptions,
 	preprocessOptions,
 }: Params) {
 	const { code } = await preprocess(
 		source,
-		[cssModules(cssOptions)],
-		preprocessOptions,
+		[cssModules(cssPreprocessorOptions)],
+		{
+			...preprocessOptions,
+			filename: path.resolve(__dirname, './test/App.svelte'),
+		},
 	);
 
 	return code;
